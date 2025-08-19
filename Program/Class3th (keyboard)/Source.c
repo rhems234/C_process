c4f486748573f5f2ee9db1d4e5217bab124d8dd5
rhems234 <rhems234@gmail.com>
2025-08-19 19:46:41 +0900

    (2025.8.19) 좌표 설정 시스템

---
+++ Program/Class3th (keyboard)/Source.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+#define SIZE 10
+
+int main() {
+
+#pragma region GetAsynckeyState
+	// Windows API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
+	// 비동적으로 확인할 때 사용되는 함수입니다.
+
+	// Key State
+
+	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태.
+
+	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태.
+
+	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태.
+
+	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태.
+
+	// 포인트 배열 - 짝수는 탐정, 홀수는 의뢰인
+
+	const char* text[SIZE] = {
+		"오랜만이네요", "오랜만이군", "무슨 일 이죠 ?", "어떻게 지내는 지 궁금해서 왔네",
+		"저야 잘 지내죠", "그렇군", "뭔가 의뢰할 게 있어보이는군요", "사실은 그렇다네",
+		"이야기하시죠", "그럼..."
+	};
+
+	int count = 0;
+
+	/*for (int i = 0; i < SIZE; i++) {
+
+		if (GetAsyncKeyState(VK_SPACE)) {
+			if (i % 2 == 0) {
+				printf("탐정 : %s\n", text[i]);
+			}
+			else {
+				printf("의뢰인 : %s\n", text[i]);
+			}
+			count++;
+		}
+
+	}*/
+
+	printf("스페이스바를 눌러 대화를 진행하세요...\n\n");
+
+	while (count < SIZE) {
+		if (GetAsyncKeyState(VK_SPACE) && 0x8001) {
+			if (count % 2 == 0) {
+				printf("탐정 : %s\n", text[count]);
+			}
+			else {
+				printf("의뢰인 : %s\n", text[count]);
+			}
+			count++;
+
+			while (GetAsyncKeyState(VK_SPACE) && 0x8001) {
+				Sleep(10);
+			}
+
+		}
+
+	}
+
+	printf("대화가 종료되었습니다...\n");
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program/Program.c
+++ Program/Program/Program.c
@@ -1,42 +1,49 @@
 ﻿#include <stdio.h>
+#include <Windows.h>
 
-#define SIZE 10
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void move(int x, int y) {
+	
+	// x와 y축을 설정하는 구조체 입니다.
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+	printf("★");
+}
 
 int main() {
 
-#pragma region GetAsynckeyState
-	// Windows API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
-	// 비동적으로 확인할 때 사용되는 함수입니다.
+	//move(5, 5);
 
-	// Key State
+	while (1) {
 
-	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태.
+		int ch = _getch();
 
-	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태.
+		
 
-	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태.
-
-	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태.
-
-	// 포인트 배열 - 짝수는 탐정, 홀수는 의뢰인
-
-	const char * text[SIZE] = { 
-		"오랜만이네요", "오랜만이군", "무슨 일 이죠 ?", "어떻게 지내는 지 궁금해서 왔네",
-		"저야 잘 지내죠", "그렇군", "뭔가 의뢰할 게 있어보이는군요", "사실은 그렇다네",
-		"이야기하시죠", "그럼..." };
-
-	
-	for (int i = 0; i < SIZE; i++) {
-		if (i % 2 == 0) {
-			printf("탐정 : %s\n", text[i]);
+		switch (ch) {
+			case UP:
+				printf("UP\n");
+				break;
+			case LEFT:
+				printf("LEFT\n");
+				break;
+			case RIGHT:
+				printf("RIGHT\n");
+				break;
+			case DOWN:
+				printf("DOWN\n");
+				break;
+			default:
+				break;
 		}
-		else {
-			printf("의뢰인 : %s\n", text[i]);
-		}
+ 
 	}
-
-#pragma endregion
-
 
 	return 0;
 }
